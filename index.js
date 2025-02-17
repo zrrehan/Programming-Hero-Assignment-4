71ffa85b014e3ef40abc18644d7d64ca40723bac
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-02-17 21:24:54 +0600

    Task 2 done

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ function cashOut(money) {
     return money * 1.75/ 100
 }
 
+function validEmail(email) {
+    function hasAt() {
+        for(let text of email) {
+            if(text === "@") {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function hasSpace() {
+        for (let text of email) {
+            if(text === " ") {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function hasDotCom() {
+        if (email.slice(email.length - 4, email.length) == ".com") {
+            return true;
+        }
+        return false;
+    }
+    if(typeof email !== "string") {
+        return "Invalid";
+    }
+
+    let condition = email[0] !== "." && email[0] !== "-" && email[0] !== "_" && email[0] !== "+" && email[0] !== "@"
+                    && hasAt() && !hasSpace() && hasDotCom();
+
+    return condition;
+}
